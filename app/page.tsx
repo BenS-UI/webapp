cbc56e76a95a37d6ef5627d10333bb354f1367e7
BenS-UI <bensandivar9@gmail.com>
2025-08-23 15:19:17 -0700

    it's alive

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main style={{ paddingTop: "calc(var(--nav-height) + 24px)", paddingInline: 16 }}>
+      <h1>Home</h1>
+      <p>It’s alive.</p>
+    </main>
+  );
+}
