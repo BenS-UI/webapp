b4dbb880555cfe130e7509bed08ee268060d63a6
BenS-UI <bensandivar9@gmail.com>
2025-08-27 18:09:03 +0200

    components and sections added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,16 +4,16 @@ export default function Navbar() {
   return (
     <nav className="navbar" aria-label="Primary">
       <div className="nav-container">
-        <Link href="/#hero" className="logo" aria-label="Home">
+        <Link href="/" className="logo" aria-label="Home">
           <img src="/images/B-logo-a.svg" alt="Ben Sandivar Logo" />
         </Link>
         <ul className="nav-links">
-          <li><Link href="/#hero">Home</Link></li>
-          <li><Link href="/#gallery">Gallery</Link></li>
-          <li><Link href="/#projects">Work</Link></li>
-          <li><Link href="/#music">Music</Link></li>
-          <li><Link href="/#about">About</Link></li>
-          <li><Link href="/#contact">Contact</Link></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/projects">Gallery</Link></li>
+          <li><Link href="/projects">Work</Link></li>
+          <li><Link href="/playground">Music</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
           <li><Link href="/playground">Playground</Link></li>
           <li><Link href="/blog">Blog</Link></li>
         </ul>
